8f8cb557732f97e90f48650c0d3d46052714a4ad
victazevedo <vict.azevedo@gmail.com>
2016-12-27 16:07:09 +1300

    Add files via upload

--- EnviroWalk/EnviroWalk.DataModel/Migrations/201612180858369_dbInitial.cs
+++ EnviroWalk/EnviroWalk.DataModel/Migrations/201612180858369_dbInitial.cs
@@ -8,59 +8,98 @@ namespace EnviroWalk.DataModel.Migrations
         public override void Up()
         {
             CreateTable(
-                "dbo.ReportModels",
+                "dbo.Report",
                 c => new
                     {
-                        ID = c.Int(nullable: false, identity: true),
+                        ReportID = c.Int(nullable: false, identity: true),
                         UserID = c.Int(nullable: false),
                         ReportFarmName = c.String(),
                         ReportDateTime = c.DateTime(nullable: false),
                     })
-                .PrimaryKey(t => t.ID);
-            
+                .PrimaryKey(t => t.ReportID);
+
             CreateTable(
-                "dbo.RepQuestionModels",
+                "dbo.Category",
+                c => new
+                {
+                    CategoryID = c.Int(nullable: false, identity: true),
+                    ReportFarmName = c.String(),
+                })
+                .PrimaryKey(t => t.CategoryID);
+
+            CreateTable(
+                "dbo.Question",
+                c => new
+                {
+                    QuestionID = c.Int(nullable: false, identity: true),
+                    QuestionDescription = c.String(),
+                    Category_ID = c.Int(),
+                })
+                .PrimaryKey(t => t.QuestionID)
+                .ForeignKey("dbo.Category", t => t.Category_ID)
+                .Index(t => t.Category_ID);
+
+            CreateTable(
+                "dbo.QuestionAction",
+                c => new
+                {
+                    QuestionActionID = c.Int(nullable: false, identity: true),
+                    QuestionActionDescription = c.String(),
+                    QuestionActionOrder = c.Int(nullable: false),
+                    Question_ID = c.Int(),
+                })
+                .PrimaryKey(t => t.QuestionActionID)
+                .ForeignKey("dbo.Question", t => t.Question_ID)
+                .Index(t => t.Question_ID);
+
+            CreateTable(
+                "dbo.RepQuestion",
                 c => new
                     {
-                        ID = c.Int(nullable: false, identity: true),
-                        QuestionID = c.Int(nullable: false),
+                        RepQuestionID = c.Int(nullable: false, identity: true),
                         RepQuestionAnswer = c.Boolean(nullable: false),
+                        Question_ID = c.Int(),
                         Report_ID = c.Int(),
                     })
-                .PrimaryKey(t => t.ID)
-                .ForeignKey("dbo.ReportModels", t => t.Report_ID)
-                .Index(t => t.Report_ID);
+                .PrimaryKey(t => t.RepQuestionID)
+                .ForeignKey("dbo.Report", t => t.Report_ID)
+                .ForeignKey("dbo.Question", t => t.Question_ID)
+                .Index(t => t.Report_ID)
+                .Index(t => t.Question_ID);
             
             CreateTable(
-                "dbo.RepQuestionActionModels",
+                "dbo.RepQuestionAction",
                 c => new
                     {
-                        ID = c.Int(nullable: false, identity: true),
-                        QuestionActionID = c.Int(nullable: false),
+                        RepQuestionActionID = c.Int(nullable: false, identity: true),
                         RepQuestionActionAns = c.Boolean(nullable: false),
                         RepQuestionActionDate = c.DateTime(nullable: false),
                         Report_ID = c.Int(),
+                        QuestionAction_ID = c.Int(),
                     })
-                .PrimaryKey(t => t.ID)
-                .ForeignKey("dbo.ReportModels", t => t.Report_ID)
-                .Index(t => t.Report_ID);
+                .PrimaryKey(t => t.RepQuestionActionID)
+                .ForeignKey("dbo.Report", t => t.Report_ID)
+                .ForeignKey("dbo.QuestionAction", t => t.QuestionAction_ID)
+                .Index(t => t.Report_ID)
+                .Index(t => t.QuestionAction_ID);
             
             CreateTable(
-                "dbo.RepQuestionCusActionModels",
+                "dbo.RepQuestionCusAction",
                 c => new
                     {
-                        ID = c.Int(nullable: false, identity: true),
-                        QuestionID = c.Int(nullable: false),
+                        RepQuestionCusActionID = c.Int(nullable: false, identity: true),
                         RepQuestionCusActionAns = c.Boolean(nullable: false),
                         RepQuestionCusActionDate = c.DateTime(nullable: false),
-                        RepQuestionAnswer = c.Boolean(nullable: false),
                         RepQuestionCusActionDescription = c.String(),
                         RepQuestionCusActionOrder = c.Int(nullable: false),
                         Report_ID = c.Int(),
+                        Question_ID = c.Int(),
                     })
-                .PrimaryKey(t => t.ID)
-                .ForeignKey("dbo.ReportModels", t => t.Report_ID)
-                .Index(t => t.Report_ID);
+                .PrimaryKey(t => t.RepQuestionCusActionID)
+                .ForeignKey("dbo.Report", t => t.Report_ID)
+                .ForeignKey("dbo.Question", t => t.Question_ID)
+                .Index(t => t.Report_ID)
+                .Index(t => t.Question_ID);
             
         }
         
@@ -76,6 +115,32 @@ namespace EnviroWalk.DataModel.Migrations
             DropTable("dbo.RepQuestionActionModels");
             DropTable("dbo.RepQuestionModels");
             DropTable("dbo.ReportModels");
+
+            DropForeignKey("dbo.RepQuestionCusAction", "Report_ID", "dbo.Report");
+            DropForeignKey("dbo.RepQuestionCusAction", "Question_ID", "dbo.Question");
+            DropForeignKey("dbo.RepQuestionAction", "Report_ID", "dbo.Report");
+            DropForeignKey("dbo.RepQuestionAction", "QuestionAction_ID", "dbo.QuestionAction");
+            DropForeignKey("dbo.RepQuestion", "Report_ID", "dbo.Report");
+            DropForeignKey("dbo.RepQuestion", "Question_ID", "dbo.Question");
+            DropForeignKey("dbo.QuestionAction", "Question_ID", "dbo.Question");
+            DropForeignKey("dbo.Question", "Category_ID", "dbo.Category");
+
+            DropIndex("dbo.RepQuestionCusAction", new[] { "Report_ID" });
+            DropIndex("dbo.RepQuestionCusAction", new[] { "Question_ID" });
+            DropIndex("dbo.RepQuestionAction", new[] { "Report_ID" });
+            DropIndex("dbo.RepQuestionAction", new[] { "QuestionAction_ID" });
+            DropIndex("dbo.RepQuestion", new[] { "Report_ID" });
+            DropIndex("dbo.RepQuestion", new[] { "Question_ID" });
+            DropIndex("dbo.QuestionAction", new[] { "Question_ID" });
+            DropIndex("dbo.Question", new[] { "Category_ID" });
+
+            DropTable("dbo.RepQuestionCusAction");
+            DropTable("dbo.RepQuestionAction");
+            DropTable("dbo.RepQuestion");
+            DropTable("dbo.Report");
+            DropTable("dbo.Question");
+            DropTable("dbo.QuestionAction");
+            DropTable("dbo.Category");
         }
     }
 }
