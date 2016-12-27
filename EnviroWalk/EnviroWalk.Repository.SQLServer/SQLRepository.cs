37147ac6117df131be1c016c4484b32f797cffb4
victazevedo <vict.azevedo@gmail.com>
2016-12-27 16:14:43 +1300

    Add files via upload

--- EnviroWalk/EnviroWalk.Repository.SQLServer/SQLRepository.cs
+++ EnviroWalk/EnviroWalk.Repository.SQLServer/SQLRepository.cs
@@ -6,51 +6,42 @@ using System.Text;
 using System.Threading.Tasks;
 using EnviroWalk.Model;
 using EnviroWalk.DataModel;
+using System.Data.Entity.Core.Objects;
 
 namespace EnviroWalk.Repository.SQLServer
 {
     public partial class SQLRepository : IReport
     {
-        public void AddReport(ReportModel report)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteReport(ReportModel report)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ReportModel GetReport(int ID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ReportModel GetLastReport(int UserID)
+        public Report GetLastReport(int UserID)
         {
             using (var context = new ContextEnviro())
             {
-                var report = context.Reports
+                var report = context.Report
                 .Include("repQuestions")
                 .Include("repQuestionActions")
                 .Include("repQuestionCusActions")
                 .Where(r => r.UserID == UserID)
-                .OrderByDescending(r => r.ID)
+                .OrderByDescending(r => r.ReportID)
                 .FirstOrDefault();
 
-                return report;
+                //var report = context.Report
+                //.Include(q => q.repQuestions)
+                //.Include("repQuestionActions")
+                //.Include("repQuestionCusActions")
+                //.Where(r => r.UserID == UserID)
+                //.OrderByDescending(r => r.ReportID)
+                //.FirstOrDefault();
+                
 
+                //((ObjectQuery)report).Include("Question");
+
+                return report ;
             }
         }
-
-        public void UpdateReport(ReportModel report)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateReports(IEnumerable<ReportModel> reports)
-        {
-            throw new NotImplementedException();
-        }
+        public Report GetReport(int ID) { throw new NotImplementedException(); }
+        public void AddReport(Report report) { throw new NotImplementedException(); }
+        public void DeleteReport(Report report) { throw new NotImplementedException(); }
+        public void UpdateReport(Report report) { throw new NotImplementedException(); }
+        public void UpdateReports(IEnumerable<Report> reports) { throw new NotImplementedException(); }
     }
 }
