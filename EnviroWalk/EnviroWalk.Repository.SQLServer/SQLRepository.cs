8a3646a885b118e1d22f2ad9f233f68d7e8b97f7
victazevedo <vict.azevedo@gmail.com>
2017-01-01 20:20:09 +1300

    Add files via upload

--- EnviroWalk/EnviroWalk.Repository.SQLServer/SQLRepository.cs
+++ EnviroWalk/EnviroWalk.Repository.SQLServer/SQLRepository.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using EnviroWalk.Model;
 using EnviroWalk.DataModel;
 using System.Data.Entity.Core.Objects;
+using Newtonsoft.Json;
+using System.Data.Entity;
 
 namespace EnviroWalk.Repository.SQLServer
 {
@@ -21,23 +23,70 @@ namespace EnviroWalk.Repository.SQLServer
                 .Include("repQuestionActions")
                 .Include("repQuestionCusActions")
                 .Where(r => r.UserID == UserID)
-                .OrderByDescending(r => r.ReportID)
+                .OrderByDescending(r => r.ReportDateTime)
                 .FirstOrDefault();
 
-                //var report = context.Report
-                //.Include(q => q.repQuestions)
-                //.Include("repQuestionActions")
-                //.Include("repQuestionCusActions")
-                //.Where(r => r.UserID == UserID)
-                //.OrderByDescending(r => r.ReportID)
-                //.FirstOrDefault();
-                
-
-                //((ObjectQuery)report).Include("Question");
-
-                return report ;
+                return report;
             }
         }
+
+        public Report SaveReport(Report report)
+        {
+            using (var context = new ContextEnviro())
+            {
+                foreach (var rqca in report.repQuestionCusActions)
+                {
+                    rqca.RepQuestionCusActionDate = DateTime.Now; //retirar essa linha de codigo depois de testar
+                    context.Entry(rqca.Question).State = EntityState.Added;
+                }
+
+                foreach (var rq in report.repQuestions)
+                    context.Entry(rq.Question).State = EntityState.Unchanged;
+
+                foreach (var rqa in report.repQuestionActions)
+                {
+                    rqa.RepQuestionActionDate = DateTime.Now; //retirar essa linha de codigo depois de testar
+                    context.Entry(rqa.QuestionAction).State = EntityState.Unchanged;
+                    //context.Entry(rqa.QuestionAction.Question).State = EntityState.Unchanged;
+                }   
+
+                report.ReportFarmName = report.ReportFarmName + " (MOD)";
+                report.ReportDateTime = DateTime.Now;
+
+                context.Report.Add(report);
+                context.SaveChanges();
+
+                var reportDoBanco = context.Report
+                .Include("repQuestions")
+                .Include("repQuestionActions")
+                .Include("repQuestionCusActions")
+                .Where(r => r.UserID == report.UserID)
+                .OrderByDescending(r => r.ReportDateTime)
+                .FirstOrDefault();
+
+                return reportDoBanco;
+            }
+        }
+
+        public Report EmailReport(int UserID, string safeEmail)
+        {
+            using (var context = new ContextEnviro())
+            {
+                var report = context.Report
+                .Include("repQuestions")
+                .Include("repQuestionCusActions")
+                .Include("repQuestionActions")
+                .Include("repQuestions.Question")
+                .Include("repQuestions.Question.Category")
+                .Include("repQuestionActions.QuestionAction")
+                .Where(r => r.UserID == UserID)
+                .OrderByDescending(r => r.ReportDateTime)
+                .FirstOrDefault(); 
+
+                return report;
+            }
+        }
+
         public Report GetReport(int ID) { throw new NotImplementedException(); }
         public void AddReport(Report report) { throw new NotImplementedException(); }
         public void DeleteReport(Report report) { throw new NotImplementedException(); }
