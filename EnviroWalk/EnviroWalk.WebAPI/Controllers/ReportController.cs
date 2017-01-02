bd73ce1e75ab04454d41749ee37e86c36df985b7
victazevedo <vict.azevedo@gmail.com>
2017-01-02 13:27:13 +1300

    Add files via upload

--- EnviroWalk/EnviroWalk.WebAPI/Controllers/ReportController.cs
+++ EnviroWalk/EnviroWalk.WebAPI/Controllers/ReportController.cs
@@ -1,8 +1,11 @@
 ﻿using EnviroWalk.Model;
 using EnviroWalk.Repository.Interface;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,14 +14,87 @@ namespace EnviroWalk.WebAPI.Controllers
 {
     public class ReportController : ApiController
     {
-        // GET: Report
-        public ReportModel Get(int id)
+        // GET Report Default
+        //Ex: localhost/api/Report/1
+        public Report Get(int id)
         {
             //report controller
-            var teste = 0;
             IReport report = RepositoryFactory.GetRepository<IReport>();
+            //var reportJson = report.GetLastReport(id);
+            //var reportString = reportJson.ToString();
+            //var retorno = JsonConvert.SerializeObject(reportJson);
             return report.GetLastReport(id);
-            
+
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("datahora")]
+        public HttpResponseMessage GetDataHoraServidor()
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("report/getlast/{id:int}")]
+        public HttpResponseMessage GetLastReport(int id)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, RepositoryFactory.GetRepository<IReport>().GetLastReport(id));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("report/save")]
+        public HttpResponseMessage SaveReport(Report report)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, RepositoryFactory.GetRepository<IReport>().SaveReport(report));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("report/email")]
+        public HttpResponseMessage EmailReport(UserTemp user)
+        {
+            try
+            {
+                /* 30/12 no final nadad disso era necessario */
+                //var retortSerial = JsonConvert.SerializeObject(report, Formatting.Indented, new JsonSerializerSettings
+                //    {
+                //        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                //        PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                //    });
+                //var response = JsonConvert.DeserializeObject<Report>(retortSerial);
+                //return Request.CreateResponse(HttpStatusCode.OK, response);
+                return Request.CreateResponse(HttpStatusCode.OK, RepositoryFactory.GetRepository<IReport>().EmailReport(user.UserID, user.UserEmail));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        public class UserTemp
+        {
+            public int UserID { get; set; }
+            public string UserEmail { get; set; }
         }
     }
-    }
+}
